ec9cdaf2ea184f4b0cde663aeaff4430c6b63dfc
haeungun <haeungun@gmail.com>
2017-09-16 14:09:09 +0900

    [이슈 #7] 카메라 버튼

--- Frontend/HackPretty/app/src/main/java/com/example/haeun_kim/hackpretty/SearchActivity.java
+++ Frontend/HackPretty/app/src/main/java/com/example/haeun_kim/hackpretty/SearchActivity.java
@@ -1,6 +1,7 @@
 package com.example.haeun_kim.hackpretty;
 
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -11,13 +12,19 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        
+
         Button btnSearch = (Button) findViewById(R.id.btnSearch);
+        Button btnCamera = (Button) findViewById(R.id.btnCamera);
+
 
         btnSearch.setOnClickListener((v) -> {
             Intent intent = new Intent(SearchActivity.this, MainActivity.class);
             startActivity(intent);
         });
 
+        btnCamera.setOnClickListener((v) -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, 1);
+        });
     }
 }
